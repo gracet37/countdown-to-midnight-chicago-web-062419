b1f5b8a6573eda0fadc31bd4a7cfa8d2e1bd6a11
Grace Tan <gracetan37@gmail.com>
2019-05-29 21:28:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(integer)
-  i = 10
-  while i >= 0
-  puts "#{i} SECOND(S)!"
-  i -= 1
+  while integer >= 0
+  puts "#{integer} SECOND(S)!"
+  integer -= 1
 end
 return "HAPPY NEW YEAR!"
 end
